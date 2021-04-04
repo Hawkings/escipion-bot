422d25134764897ed844324d6c836d14f0b8ec0e
Hawkings <daniel.marin.94@gmail.com>
2021-04-04 11:21:24 +0200

    add https

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,21 @@
 import next from 'next';
-import {createServer} from 'http';
+import {createServer, ServerOptions} from 'https';
 import {parse} from 'url';
-import {URL_BASE, WEB_PORT} from '../secret';
+import {TLS_CERT_PATH, TLS_KEY_PATH, URL_BASE, WEB_PORT} from '../secret';
+import fs from 'fs';
 
 export async function startNextServer() {
 	const dev = process.env.NODE_ENV !== 'production';
 	const app = next({dev});
 	const handle = app.getRequestHandler();
 
+	const options: ServerOptions = {
+		key: fs.readFileSync(TLS_KEY_PATH),
+		cert: fs.readFileSync(TLS_CERT_PATH),
+	};
+
 	await app.prepare();
-	createServer((req, res) => {
+	createServer(options, (req, res) => {
 		const parsedUrl = parse(req.url!, true);
 
 		handle(req, res, parsedUrl);
