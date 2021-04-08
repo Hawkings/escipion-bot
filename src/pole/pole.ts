8a63f4a6d31a995dfaf0f6d43775eddcdd5e925b
Hawkings <daniel.marin.94@gmail.com>
2021-04-08 23:14:17 +0200

    prevent pole spam

--- src/pole/pole.ts
+++ src/pole/pole.ts
@@ -25,8 +25,25 @@ const failedPoleMessage: Record<Exclude<SavePoleStatus, SavePoleStatus.SUCCESS>,
 	[SavePoleStatus.WRONG_TIME]: 'A esta hora no hay poles que valgan',
 };
 
+const poleCache = new Map<Group, Map<User, number>>();
+
 export async function pole(user: User, group: Group): Promise<void> {
 	const now = new Date();
+	if (poleCache.get(group)?.has(user)) {
+		const lastTime = poleCache.get(group)!.get(user)!;
+		const elapsed = now.getTime() - lastTime;
+		if (elapsed <= 5000) {
+			bot().sendMessage(
+				group,
+				`${await getUserName(group, user)}, no vale hacer spam. Espera ${Math.round(
+					(5000 - elapsed) / 1000,
+				)} segundos.`,
+			);
+			return;
+		}
+	}
+	if (!poleCache.has(group)) poleCache.set(group, new Map());
+	poleCache.get(group)?.set(user, now.getTime());
 	const result = maybeSavePole(user, group, now);
 	setImmediate(onPole, user, group, result, now);
 	let message: string;
