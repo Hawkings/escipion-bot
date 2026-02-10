66c7b597fe1982db2371e03358ea0df678605885
Hawkings <daniel.marin.94@gmail.com>
2026-02-10 17:32:46 +0100

    add more canary seasons

--- src/seasons/seasons.ts
+++ src/seasons/seasons.ts
@@ -1,9 +1,16 @@
 import {getCurrentSeasonStart} from '../util';
 import {SeasonType} from './types';
 
+const FEBRUARY = 1;
+const MARCH = 2;
 const APRIL = 3;
 
-const nonNormalSeasons = new Map([[new Date(2021, APRIL, 26).getTime(), SeasonType.CANARY]]);
+const nonNormalSeasons = new Map([
+	[new Date(2021, APRIL, 26).getTime(), SeasonType.CANARY],
+	[new Date(2026, FEBRUARY, 9).getTime(), SeasonType.CANARY],
+	[new Date(2026, FEBRUARY, 23).getTime(), SeasonType.CANARY],
+	[new Date(2026, MARCH, 9).getTime(), SeasonType.CANARY],
+]);
 
 export function getSeasonType(season: Date) {
 	if (nonNormalSeasons.has(season.getTime())) {
